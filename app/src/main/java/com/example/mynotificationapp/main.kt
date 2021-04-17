e2ce9dcb0c2d4e3f4610587e5e67acd54478c88a
Paul-wiseman <paulndibe92@gmail.com>
2021-04-17 11:11:55 +0100

    An App that sends notification and opens a new activity

---
+++ app/src/main/java/com/example/mynotificationapp/main.kt
@@ -0,0 +1,21 @@
+package com.example.mynotificationapp
+
+fun main() {
+
+
+    fun birthdayCakeCandles(candles: Array<Int>) {
+        var sortedCandles = candles.sorted()
+println(sortedCandles)
+        println((1,))
+        var count = 0
+//        for(i in sortedCandles){
+//            var highest = sortedCandles.last()
+//            if(i == highest){
+//                count++
+//            }
+//        }
+//        return count
+    }
+
+    birthdayCakeCandles(arrayOf(3, 2, 1, 3))
+}
